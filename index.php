5e402d917b165dbde01c638e3f5ff55e1b714809
dmitry <zrayev@mail.ru>
2015-10-23 16:30:07 +0300

    v6

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'vendor/autoload.php';
 use Footwear\Shoes;
 use Footwear\Slippers;
 
-echo '-Shoes Villomi'."<br>\n";
+echo '-Shoes Villomi' . "<br>\n";
 $villomi = new Shoes();
 $villomi
     ->setName('Villomi')
@@ -13,25 +13,23 @@ $villomi
     ->setMaterial('шкіра крокодила')
     ->setSeason('літо')
     ->setShoelace('-');
-echo $villomi->printScreen()."<br>\n";
+echo $villomi->printScreen() . "<br>\n";
 
-echo '-Shoes Patriot'."<br>\n";
+echo '-Shoes Patriot' . "<br>\n";
 $patriot = new Shoes();
 $patriot
     ->setName('Патріот')
     ->setSize('44')
     ->setMaterial('шкіра натуральна')
     ->setSeason('весна/осінь')
-    ->setShoelace('+')
-;
-echo $patriot->printScreen()."<br>\n";
+    ->setShoelace('+');
+echo $patriot->printScreen() . "<br>\n";
 
-echo '-Slippers Slippers'."<br>\n";
+echo '-Slippers Slippers' . "<br>\n";
 $slippers = new Slippers();
 $slippers
     ->setName('Тапки Monkey')
     ->setSize('38')
     ->setMaterial('махрові')
-    ->setType('закриті')
-;
-echo $slippers->printScreen()."<br>\n";
+    ->setType('закриті');
+echo $slippers->printScreen() . "<br>\n";
